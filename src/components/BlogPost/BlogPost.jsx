55620b403920f7f1779c1ac522780c4eb31519f1
Becky Rushton <beckyrushton30@gmail.com>
2023-02-19 17:22:27 +0000

    added content to blog posts and new component of blog post to import

---
+++ src/components/BlogPost/BlogPost.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./BlogPost.scss";
+
+const BlogPost = () => {
+  return (
+    <div className="blog-post-section">
+      <div className="blog-post">
+        <div className="blog-post__title">title</div>
+        <div className="blog-post__body">body</div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
--- src/containers/BlogPosts/BlogPosts.jsx
+++ src/containers/BlogPosts/BlogPosts.jsx
@@ -2,14 +2,37 @@ import React from "react";
 import "./BlogPosts.scss";
 import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
+import BlogPost from "../../components/BlogPost/BlogPost";
 
 const BlogPosts = () => {
   return (
     <div className="blog-section">
+      <div className="blog-section-top">
+        <div className="blog-section__title">Blog Posts</div>
+        <div className="blog-section__new">
+          <div className="blog-section__new--button">
+            <Link
+              to={"/"}
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <Button buttonText={"New Post"}></Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="blog-section__post">
+        <BlogPost />
+      </div>
       <div className="blog-section__home">
-        <Link to={"/"} style={{ color: "inherit", textDecoration: "inherit" }}>
-          <Button buttonText={"Home"}></Button>
-        </Link>
+        <div className="blog-section__home--button">
+          <Link
+            to={"/"}
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <Button buttonText={"Home"}></Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
